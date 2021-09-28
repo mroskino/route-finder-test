1d815fbfc3491acd6bf7f772f7813e490ca2e1f4
mroskino <michal.mrosko@gmail.com>
2021-09-28 12:07:07 +0200

    Project init

---
+++ src/test/java/com/mroskino/routefindertest/RouteFinderTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mroskino.routefindertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteFinderTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
